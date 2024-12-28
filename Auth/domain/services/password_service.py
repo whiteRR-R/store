d4883da32225aa6cd0f1258b41463e86eeec95ed
whiteRR <whiterr2134@gmail.com>
2024-12-28 18:24:14 +0500

    created a domain password security service

---
+++ Auth/domain/services/password_service.py
@@ -0,0 +1,17 @@
+import bcrypt
+
+
+class PasswordSecurityService:
+    """Сервис для хэширования паролей и проверки пароля."""
+    
+    def get_hash_password(self, password: str):
+        """Хэширование пароля с использованием bcrypt."""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password=password.encode("utf-8"), salt=salt)
+        return hashed_password.decode("utf-8")
+    
+    def verify_password(stored_hash: str, password: str) -> bool:
+        """Проверка пароля."""
+        return bcrypt.checkpw(password.encode('utf-8'), stored_hash.encode('utf-8'))
+    
+
