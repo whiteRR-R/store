172644f920aa89ff179127f6380604234f4acbc3
whiteRR <whiterr2134@gmail.com>
2024-12-28 19:13:26 +0500

    deleted custom type

--- Auth/infrastructure/persistence/models/user_model.py
+++ Auth/infrastructure/persistence/models/user_model.py
@@ -1,15 +1,14 @@
-from infrastructure.persistence.types.role_type import RoleType
-from domain.valueobject.role import Role
 from infrastructure.persistence.database import Base
 from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy import Integer, String
 
-class UserModel(Base):
-    __tablename__ = "user_model"
 
+class UserModel(Base):
+    __tablename__ = "users"
+    
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
-    role: Mapped[Role] = mapped_column(RoleType, nullable=False)
-    email: Mapped[str] = mapped_column(String(255), nullable=False)
-    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    role: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
  
