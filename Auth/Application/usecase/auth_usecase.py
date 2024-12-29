b4ecc3de3eee6162ec6216a021035edd96547944
whiteRR <whiterr2134@gmail.com>
2024-12-29 15:04:38 +0500

    created auth use-case

---
+++ Auth/Application/usecase/auth_usecase.py
@@ -0,0 +1,17 @@
+from domain.abstacts.services.auth_service import AuthServiceInterface
+
+
+class AuthUseCase:
+    def __init__(self, auth_service: AuthServiceInterface):
+        self.auth_service = auth_service
+    
+    async def register(self, username: str, role: str, email: str, password: str):
+        await self.auth_service.register_user(
+            username=username, role=role, email=email, password=password
+        )
+    
+    async def login(self, username: str, password: str):
+        try:
+            await self.auth_service.login_user(username=username, password=password)
+        except Exception:
+            raise ValueError("Invalid credentials")
