048597bf2d65faaf4c7b1229fc00f2846d8cf65b
whiteRR <whiterr2134@gmail.com>
2024-12-29 14:11:27 +0500

    created auth service

---
+++ Auth/Application/services/auth_service.py
@@ -0,0 +1,48 @@
+from domain.abstacts.uow.base_uow import BaseUnitOfWork
+from domain.services.password_service import PasswordSecurityService
+from domain.entities.user import User
+from domain.valueobject.username import Username
+from domain.valueobject.email import Email
+from domain.valueobject.role import Role
+
+
+
+class AuthService:
+    """Сервис для управления регистрацией и аутентификацией пользователей."""
+    def __init__(self, uow: BaseUnitOfWork, password_service: PasswordSecurityService):
+        self.uow = uow
+        self.password_service = password_service
+    
+    async def register_user(self, username: str, role: str, email: str, password: str):
+        """
+        Регистрация нового пользователя. Проверяет, существует ли уже пользователь с таким именем, 
+        и если нет, создает нового пользователя.
+        """
+        async with self.uow:
+            if await self.uow.auth_repository.find_by_username(username):
+                raise ValueError("Username already exist")
+            
+            username_vo = Username(username)
+            email_vo = Email(email)
+            role_vo = Role(role)
+            hash_password = self.password_service.get_hash_password(password)
+            new_user = User(
+                username=username_vo,
+                role=role_vo, 
+                email=email_vo, 
+                password_hash=hash_password
+            )
+            await self.uow.auth_repository.create(new_user)
+            await self.uow.commit(new_user)
+    
+    async def login_user(self, username: str, password: str):
+        """
+        Логин пользователя. Проверяет правильность имени пользователя и пароля.
+        """
+        async with self.uow:
+            user = await self.uow.auth_repository.find_by_username(username)
+            if not user and not self.password_service.verify_password(user.password_hash, password):
+                raise ValueError("Username or password incorrect")
+            return user
+    
+
