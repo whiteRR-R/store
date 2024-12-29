7f8f787be457886768d26941cde25bedce1f2197
whiteRR <whiterr2134@gmail.com>
2024-12-29 12:56:19 +0500

    created sqlalchemy auth repository

---
+++ Auth/infrastructure/persistence/repository/sqlalchemy_repository.py
@@ -0,0 +1,29 @@
+from domain.entities.user import User
+from domain.abstacts.repository.base_repository import BaseRepository
+from infrastructure.persistence.models.user_model import UserModel
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Optional
+
+
+class SqlAlchemyAuthRepository(BaseRepository):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+    
+    async def create(self, user: User) -> UserModel:
+        new_user = UserModel(
+            username=user.username,
+            role=user.role,
+            email=user.email,
+            password_hash=user.password_hash
+        )
+        self.session.add(new_user)
+        
+    async def find_by_username(self, username: str) -> Optional[UserModel]:
+        stmt = await self.session.execute(
+            select(UserModel).where(UserModel.username == username)
+        )
+        user = stmt.one_or_none()
+        return user
+           
+        
