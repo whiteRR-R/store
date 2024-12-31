1785ed88520e571bb41162dedfa158a9b5d6702b
whiteRR <whiterrs.zzz@gmail.com>
2025-01-01 03:49:33 +0500

    created auth controller

---
+++ Auth/presentation/http/controllers/auth_controller.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from domain.abstacts.usecases.auth_use_case import AuthUseCaseInterface
+from application.dtos.user_register_dto import UserRegisterRequest
+
+
+class AuthController:
+    def __init__(self, auth_use_case: AuthUseCaseInterface):
+        self.auth_use_case = auth_use_case
+        self.router = APIRouter()
+        
+    async def register(self, user: UserRegisterRequest):
+        await self.auth_use_case.register(
+            username=user.username,
+            role=user.role,
+            email=user.email,
+            password=user.password
+        )
+
+        return "User successfully created"
