b3870d490a3b310d773cb88b57ee275bd1b85ef0
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-20 15:51:35 +0500

    renamed the name of the abstract base interface

--- Auth/domain/abstacts/repository/base_repository.py
+++ Auth/domain/abstacts/repository/base_repository.py
@@ -1,13 +1,6 @@
 from abc import ABC, abstractmethod
 
 
-class BaseRepository(ABC):
-    @abstractmethod
-    async def create(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    async def update(self):
-        raise NotImplementedError
-
+class IBaseRepository(ABC):
+    ...
     
