0ea8e859ca3d2dd80d78a649dde25a27b233bdc4
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-20 23:35:09 +0500

    added settings for  jwt security

--- Auth/config.py
+++ Auth/config.py
@@ -10,8 +10,15 @@ class DatabaseSettings:
     database_url: str = os.getenv("DATABASE_URL")
 
 
+class JWTSecuritySettings:
+    private_key: Path = BASE_DIR / "certs" / "private_key.pem"
+    public_key: Path = BASE_DIR / "certs" / "public_key.pem"
+    alghoritm: str = "RS256"
+
+
 class ConfigManager:
     database_settings: DatabaseSettings = DatabaseSettings()
+    jwt_settings: JWTSecuritySettings = JWTSecuritySettings()
 
 
 config_manager = ConfigManager()
