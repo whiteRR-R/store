47bccfeb81dc2be240768cae0eead21f06d50266
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-21 17:55:59 +0500

    fixed error

--- Auth/application/usecase/auth_usecase.py
+++ Auth/application/usecase/auth_usecase.py
@@ -6,12 +6,12 @@ class AuthUseCase:
         self.auth_service = auth_service
     
     async def register(self, username: str, role: str, email: str, password: str):
-        await self.auth_service.register_user(
+        return await self.auth_service.register_user(
             username=username, role=role, email=email, password=password
         )
     
     async def login(self, username: str, password: str):
         try:
-            await self.auth_service.login_user(username=username, password=password)
+            return await self.auth_service.login_user(username=username, password=password)
         except Exception:
             raise ValueError("Invalid credentials")
