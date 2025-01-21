a919fd011e8b048ef5bc8eaf75898c4222307512
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-21 17:54:09 +0500

    added login response dto

--- Auth/application/dtos/login_dto.py
+++ Auth/application/dtos/login_dto.py
@@ -5,3 +5,9 @@ class UserLoginRequest(BaseModel):
     username: str
     password: str
 
+
+class UserLoginResponse(BaseModel):
+    username: str
+    access_token: str
+    refresh_token: str
+    token_type: str = "Bearer"
