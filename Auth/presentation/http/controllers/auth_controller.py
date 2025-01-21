e3338a2f72dfd89e74423932363e85469b1871a8
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-21 18:05:06 +0500

    added logic for login method

--- Auth/presentation/http/controllers/auth_controller.py
+++ Auth/presentation/http/controllers/auth_controller.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from domain.interface.usecases.auth_use_case import AuthUseCaseInterface
-from Auth.application.dtos.register_dto import UserRegisterRequest
+from application.dtos.register_dto import UserRegisterRequest, UserRegisterResponse
+from application.dtos.login_dto import UserLoginRequest, UserLoginResponse
 
 
 class AuthController:
@@ -8,12 +9,32 @@ class AuthController:
         self.auth_use_case = auth_use_case
         self.router = APIRouter()
         
-    async def register(self, user: UserRegisterRequest):
+        self.router.add_api_route(
+            "/register",
+            self.register,
+            methods=["POST"],
+            response_model=UserRegisterResponse
+        )
+        self.router.add_api_route(
+            "/login",
+            self.login,
+            methods=["POST"],
+            response_model=UserLoginResponse
+        )
+        
+    async def register(self, user: UserRegisterRequest) -> UserRegisterResponse:
         await self.auth_use_case.register(
             username=user.username,
             role=user.role,
             email=user.email,
             password=user.password
         )
-
-        return "User successfully created"
+        return UserRegisterResponse()
+    
+    async def login(self, user: UserLoginRequest) -> UserLoginResponse:
+        user_data = await self.auth_use_case.login(username=user.username, password=user.password)
+        return UserLoginResponse(
+            username=user.username,
+            access_token=user.access_token,
+            refresh_token=user.refresh_token,
+            )
