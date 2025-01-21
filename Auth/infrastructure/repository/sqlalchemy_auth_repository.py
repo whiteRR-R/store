7519a643d992447ffbae5e11da994c8a1175825d
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-21 17:34:34 +0500

    added method information

--- Auth/infrastructure/repository/sqlalchemy_auth_repository.py
+++ Auth/infrastructure/repository/sqlalchemy_auth_repository.py
@@ -7,12 +7,12 @@ from typing import Optional
 
 
 class SqlAlchemyAuthRepository(ISqlAlchemyAuthRepository):
-    """Инициализация Sqlalchemy Auth Repository репозитория."""
+    """ Инициализация Sqlalchemy Auth Repository репозитория. """
     def __init__(self, session: AsyncSession):
         self.session = session
     
     async def create(self, user: User) -> UserModel:
-        """Создает нового пользователя в базе данных."""
+        """ Создает нового пользователя в базе данных. """
         new_user = UserModel(
             username=user.username,
             role=user.role,
@@ -22,7 +22,7 @@ class SqlAlchemyAuthRepository(ISqlAlchemyAuthRepository):
         self.session.add(new_user)
         
     async def find_by_username(self, username: str) -> Optional[UserModel]:
-        """Находит пользователя по его имени (username)."""
+        """ Находит пользователя по его имени (username). """
         stmt = await self.session.execute(
             select(UserModel).where(UserModel.username == username)
         )
@@ -30,6 +30,7 @@ class SqlAlchemyAuthRepository(ISqlAlchemyAuthRepository):
         return user
     
     async def find_by_email(self, email: str) -> Optional[UserModel]:
+        """ Находит пользователя по его почте (email). """
         stmt = await self.session.execute(
             select(UserModel).where(UserModel.email == email)
         )
