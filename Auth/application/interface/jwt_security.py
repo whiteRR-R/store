2e539459dd47220749d726c718a9e2f8282c74dd
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-21 14:26:46 +0500

    added parameter for jwt service methods

--- Auth/application/interface/jwt_security.py
+++ Auth/application/interface/jwt_security.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
+from datetime import  timedelta
 
 
 class JWTSecurityInterface(ABC):
     @abstractmethod
-    def encode_jwt(self):
+    def encode_jwt(self, payload: dict, expire_timedelta: timedelta | None):
         raise NotImplementedError
     
     @abstractmethod
-    def decode_jwt(self):
+    def decode_jwt(self, jwt_token: str):
         raise NotImplementedError
     
