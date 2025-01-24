15baf397631d45e94612b003454ce94a18b9b775
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-24 13:23:32 +0500

    updated interface

--- Auth/domain/interface/repository/sqlalchemy_auth_repository.py
+++ Auth/domain/interface/repository/sqlalchemy_auth_repository.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
+from domain.entities.user import User
 from domain.interface.repository.base_repository import IBaseRepository
 
 
 class SqlAlchemyAuthRepositoryInterface(IBaseRepository, ABC):
     @abstractmethod
-    async def create(self):
+    async def create(self, user: User):
         raise NotImplementedError
     
     @abstractmethod
-    async def find_by_username(self):
+    async def find_by_username(self, username: str):
         raise NotImplementedError
     
+    async def find_by_email(self, email: str):
+        raise NotImplementedError
