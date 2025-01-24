fe5a074383abfbc73e03b4eddbe3d1399eb4527f
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-24 12:30:52 +0500

    added parameters for methods

--- Auth/application/interface/security/password_security.py
+++ Auth/application/interface/security/password_security.py
@@ -3,9 +3,9 @@ from abc import ABC,abstractmethod
 
 class PasswordSecurityInterface(ABC):
     @abstractmethod
-    def get_hash_password(self):
+    def get_hash_password(self, password: bytes) -> bytes:
         raise NotImplementedError
     
     @abstractmethod
-    def verify_password(self):
+    def verify_password(self, password: bytes, stored_hash: bytes) -> bool:
         raise NotImplementedError
