03d1280637d1ed27506522d89e65a70a3bfdbe14
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-25 17:53:11 +0500

    added a new method to get information about the current user and also used a custom exception

--- Auth/application/services/auth_service.py
+++ Auth/application/services/auth_service.py
@@ -7,6 +7,7 @@ from domain.valueobject.role import Role
 from domain.interface.services.jwt_service import JWTServiceInterface
 from application.interface.security.password_security import PasswordSecurityInterface
 from application.dtos.jwt_token_dto import JWTTokens
+from application.exceptions import AlreadyExistsException, AuthenticationException, UserNotFoundException, InvalidTokenException
 from datetime import timedelta
 
 
@@ -55,12 +56,24 @@ class AuthService(AuthServiceInterface):
             user = await self.uow.repository.find_by_username(username)
             
             if not user:
-                raise ValueError("Username or password not correct")
+                raise AuthenticationException("Username or password not correct")
             if not self.password_security.verify_password(password, user.hashed_password):
-                raise ValueError("Username or password not correct")
+                raise AuthenticationException("Username or password not correct")
             
             payload = {"sub": username}
             access_token = await self.jwt_service.create_access_token(payload)
             refresh_token = await self.jwt_service.create_refresh_token(payload)
             return JWTTokens(access_token=access_token, refresh_token=refresh_token)
-
+    
+    async def get_current_user_info(self, jwt_token: str):
+        """ Возврашает информацию текущего пользователя """
+        async with self.uow:
+            subject_name = self.jwt_service.get_token_subject(jwt_token)
+            user = self.uow.repository.find_by_username(subject_name)
+            
+            if user is None:
+                raise UserNotFoundException("User not found or invalid credentials")
+            
+            return user
+                
+    
