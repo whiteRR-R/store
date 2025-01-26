3a94f9086bdbe5e81b47b79a2c2f03bc6acaa2c0
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-26 17:27:52 +0500

    added a new endpoint that receives information about the current user

--- Auth/presentation/http/controllers/auth_controller.py
+++ Auth/presentation/http/controllers/auth_controller.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends, Form
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from domain.interface.usecases.auth_use_case import AuthUseCaseInterface
 from application.dtos.register_dto import UserRegisterRequest, UserRegisterResponse
 from application.dtos.login_dto import UserLoginRequest, UserLoginResponse
 
 
+
 class AuthController:
+    oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+    
     def __init__(self, auth_use_case: AuthUseCaseInterface):
         self.auth_use_case = auth_use_case
         self.router = APIRouter()
@@ -22,6 +26,12 @@ class AuthController:
             response_model=UserLoginResponse
         )
         
+        self.router.add_api_route(
+            "/me",
+            self.get_user_data,
+            methods=["GET"],
+        )
+        
     async def register(self, user: UserRegisterRequest) -> UserRegisterResponse:
         try:
             print(type(user.password))
@@ -36,10 +46,17 @@ class AuthController:
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     
-    async def login(self, user: UserLoginRequest) -> UserLoginResponse:
-        user_tokens = await self.auth_use_case.login(username=user.username, password=user.password)
+    async def login(
+        self,
+        username: str = Form(),
+        password: bytes = Form()
+    ) -> UserLoginResponse:
+        user_tokens = await self.auth_use_case.login(username=username, password=password)
         return UserLoginResponse(
-            username=user.username,
             access_token=user_tokens.access_token,
             refresh_token=user_tokens.refresh_token,
-            )
+        )
+ 
+    async def get_user_data(self, jwt_token: str=Depends(oauth2_scheme)):
+        user_info = await self.auth_use_case.get_current_user_info(jwt_token)
+        return user_info
