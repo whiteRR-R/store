10927fdc4306cfdfcb2cc60611badad97f0d661a
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-27 19:41:25 +0500

    updated dtos

--- Auth/application/dtos/jwt_token_dto.py
+++ Auth/application/dtos/jwt_token_dto.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
 
 
+class JWTTokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str | None = None
+    token_type: str = "Bearer"
+
+
 class JWTTokens(BaseModel):
     access_token: str | bytes
     refresh_token: str | bytes
+    
--- Auth/application/dtos/login_dto.py
+++ Auth/application/dtos/login_dto.py
@@ -4,9 +4,3 @@ from pydantic import BaseModel
 class UserLoginRequest(BaseModel):
     username: str
     password: str
-
-
-class UserLoginResponse(BaseModel):
-    access_token: str
-    refresh_token: str
-    token_type: str = "Bearer"
