8b9b8055e2dbfde683eeb52de436cca523aaaa0c
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-27 20:12:41 +0500

    created http exception handle

---
+++ Auth/presentation/decorators/http_exception.py
@@ -0,0 +1,16 @@
+from fastapi import HTTPException, status
+from application.exceptions import *
+from functools import wraps
+
+def handle_http_exception(func):
+    @wraps(func)  # Сохраняем метаданные оригинальной функции
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except RegistrationException as exception:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(exception))
+        except AuthenticationException as exception:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exception))
+        except TokenProcessingException as exception:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exception))
+    return wrapper
