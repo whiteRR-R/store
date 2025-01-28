cd60c6fc51f04ea873a013926ff6cd1c889501c1
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-28 16:17:12 +0500

    added new value for reset token

--- Auth/config.py
+++ Auth/config.py
@@ -14,8 +14,10 @@ class JWTSettings:
     private_key: Path = BASE_DIR / "certs" / "private_key.pem"
     public_key: Path = BASE_DIR / "certs" / "public_key.pem"
     alghoritm: str = "RS256"
+    reset_token_expire_time_minute = 10
     access_token_expire_time_minute = 15
     refresh_token_expire_time_day = 20
+    reset_token_type = 'reset'
     access_token_type = 'access'
     refresh_token_type = 'refresh'
 
