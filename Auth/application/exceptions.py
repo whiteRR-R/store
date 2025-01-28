3c47df5e994c26efc6a95f95e392812a722d9b39
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-28 16:14:06 +0500

    updated exception

--- Auth/application/exceptions.py
+++ Auth/application/exceptions.py
@@ -1,18 +1,18 @@
-class AlreadyExistsException(Exception):
-    """ Исключение для случаем если уже существует какая небудь сущность """
-    pass
-
-class AuthenticationException(Exception):
+class AuthException(Exception):
     """ Исключение для ошибок аутентификации """
-    pass
-
-class UserNotFoundException(Exception):
-    """ Исключение если пользователь не найден """
     pass
 
 class RegistrationException(Exception):
     """ Исключение для ошибок регистрации """
     pass
 
-class TokenProcessingException(Exception):
+class AlreadyExistsException(RegistrationException):
+    """ Исключение для случаем если уже существует какая небудь сущность """
+    pass
+
+class UserNotFoundException(AuthException):
+    """ Исключение если пользователь не найден """
+    pass
+
+class TokenProcessingException(AuthException):
     pass
--- Auth/presentation/decorators/http_exception.py
+++ Auth/presentation/decorators/http_exception.py
@@ -9,8 +9,6 @@ def handle_http_exception(func):
             return await func(*args, **kwargs)
         except RegistrationException as exception:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(exception))
-        except AuthenticationException as exception:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exception))
-        except TokenProcessingException as exception:
+        except AuthException as exception:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exception))
     return wrapper
