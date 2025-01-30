853c437d93f93bdc1c1c1b215728b0ffaa836cc8
whiteRR-R <whiterrs.zzz@gmail.com>
2025-01-30 14:40:31 +0500

    added statisticnod to convert ORM model to domain model

--- Auth/infrastructure/persistence/models/user_model.py
+++ Auth/infrastructure/persistence/models/user_model.py
@@ -1,3 +1,7 @@
+from domain.entities.user import User
+from domain.valueobject.role import Role
+from domain.valueobject.email import Email
+from domain.valueobject.username import Username
 from infrastructure.persistence.database import Base
 from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy import Integer, String, LargeBinary
@@ -11,4 +15,19 @@ class UserModel(Base):
     role: Mapped[str] = mapped_column(String, nullable=False)
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     hashed_password: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
- 
+    
+    def __init__(self, user: User):
+        """ Маппинг из доменной модели в ORM-модель. """
+        self.username = user.username
+        self.role = user.role
+        self.email = user.email
+        self.hashed_password = user.hash_password
+    
+    @staticmethod
+    def to_domain(self) -> User:
+        return User(
+            username=Username(self.username),
+            role=Role(self.role),
+            email=Email(self.email),
+            hash_password=self.hashed_password
+        )
