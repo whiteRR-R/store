b22c978b2322d3aec89baac2ebb006825d045381
whiteRR-R <whiterrs.zzz@gmail.com>
2025-02-03 19:10:27 +0500

    updated module name

--- Auth/domain/interface/usecases/auth_usecase.py
+++ Auth/domain/interface/usecases/auth_usecase.py
@@ -17,3 +17,13 @@ class AuthUseCaseInterface(ABC):
     @abstractmethod
     async def generate_access_token_from_refresh(self, jwt_token: str):
         raise NotImplementedError
+    
+    @abstractmethod
+    async def forgot_password(self, email: str):
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def reset_password(self, jwt_token: str, new_password: bytes):
+        raise NotImplementedError
+    
+    
