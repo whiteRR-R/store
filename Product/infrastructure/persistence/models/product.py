36f259f2d3231a2eb2c19c133f625ba0d0d7199a
whiteRR-R <whiterrs.zzz@gmail.com>
2025-02-15 13:23:29 +0500

    added dunder init method

--- Product/infrastructure/persistence/models/product.py
+++ Product/infrastructure/persistence/models/product.py
@@ -1,7 +1,11 @@
-from infrastructure.persistence.database import Base
 from decimal import Decimal
-from sqlalchemy.orm import Mapped, mapped_column
+from math import prod
+from typing import Any
 from sqlalchemy import String, DECIMAL, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from infrastructure.persistence.database import Base
+from domain.entities.product import Product
+
 
 class ProductModel(Base):
     __tablename__ = 'product'
@@ -11,4 +15,11 @@ class ProductModel(Base):
     category: Mapped[str] = mapped_column(String, nullable=False)
     price: Mapped[Decimal] = mapped_column(DECIMAL, nullable=False)
     quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    def __init__(self, product: Product):
+        self.name = product.name
+        self.description = product.description
+        self.category = product.category.name
+        self.price = product.price
+        self.quantity = product.quantity
     
