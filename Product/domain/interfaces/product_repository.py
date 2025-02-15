7f30bb2415d52f936b027595e5574032f90aeac9
whiteRR-R <whiterrs.zzz@gmail.com>
2025-02-15 13:22:54 +0500

    added parameters for methods

--- Product/domain/interfaces/product_repository.py
+++ Product/domain/interfaces/product_repository.py
@@ -1,24 +1,26 @@
 from abc import ABC, abstractmethod
+from domain.entities.product import Product
 
 
 class ProductRepositoryInterface(ABC):
     @abstractmethod
-    async def create(self):
+    async def create(self, product: Product):
         raise NotImplementedError
     
     @abstractmethod
-    async def update(self):
+    async def update(self, product: Product):
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def get_all(self):
         raise NotImplementedError
     
     @abstractmethod
     async def find_by_name(self, name: str):
         raise NotImplementedError
 
-    @abstractmethod
-    async def get_all(self):
-        raise NotImplementedError
     
     @abstractmethod
-    async def find_by_category(self):
+    async def find_by_category(self, category: str):
         raise NotImplementedError
     
