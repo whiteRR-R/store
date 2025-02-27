602b851b4d710ef12dbea42d793bd19a3b20a558
whiteRR-R <whiterrs.zzz@gmail.com>
2025-02-27 10:29:27 +0500

    updated function name

--- Auth/application/helpers/dtos.py
+++ Auth/application/helpers/dtos.py
@@ -2,7 +2,7 @@ from application.dtos.user_dto import UserDTO
 from domain.entities.user import User
 
 
-def user_dtos_to_user_entitiy(user_dto: UserDTO) -> User:
+def user_dto_to_user_entity(user_dto: UserDTO) -> User:
     username = user_dto.username
     email = user_dto.email
     role = user_dto.role
