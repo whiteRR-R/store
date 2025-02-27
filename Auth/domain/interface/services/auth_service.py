1e5ea59810aac62f37cc52fe5c9ab48ce6c3a556
whiteRR-R <whiterrs.zzz@gmail.com>
2025-02-27 10:31:33 +0500

    updated interface

--- Auth/domain/interface/services/auth_service.py
+++ Auth/domain/interface/services/auth_service.py
@@ -1,18 +1,19 @@
-from domain.entities.user import User
 from abc import abstractmethod, ABC
+from application.dtos.login_dto import UserLoginDTO
+from application.dtos.user_dto import UserDTO
 
 
 class AuthServiceInterface(ABC):
     @abstractmethod
-    async def create_user(self, user: User):
+    async def create_user(self, user_dto: UserDTO):
         raise NotImplementedError
     
     @abstractmethod
-    async def existing_username_or_email(self, username: str, email: str) -> str:
+    async def _existing_username_or_email(self, username: str, email: str) -> str:
         raise NotImplementedError
 
     @abstractmethod
-    async def verify_user_credentials(self, username: str, password: str):
+    async def verify_user_credentials(self, user_credentials: UserLoginDTO):
         raise NotImplementedError
     
     @abstractmethod
