3de5882f52941b795dbe1fb48eda71d6dc894006
whiteRR-R <whiterrs.zzz@gmail.com>
2025-03-03 10:02:08 +0500

    updated variables

--- Auth/container.py
+++ Auth/container.py
@@ -5,7 +5,7 @@ from application.usecase.auth_usecase import AuthUseCase
 from application.services.auth_service import AuthService
 from infrastructure.persistence.database import Database
 from infrastructure.services.jwt_service import JWTService
-from infrastructure.uow.sqlalchemy_uow import SqlAlchemyUnitOfWork
+from infrastructure.uow.uow import UnitOfWork
 from infrastructure.security.jwt_security import JWTSecurity
 from infrastructure.security.password_security import PasswordSecurity
 
@@ -17,9 +17,9 @@ class Container(containers.DeclarativeContainer):
     session = providers.Resource(lambda database: database.get_session(), database=database)
     print(session)
     print(type(session))
-    sqlalchemy_uow = providers.Factory(
-        SqlAlchemyUnitOfWork,
-        session=session,  # Передаем провайдер ресурса session
+    unit_of_work = providers.Factory(
+        UnitOfWork,
+        session=session,
     )
     
     jwt_security = providers.Singleton(JWTSecurity)
@@ -28,7 +28,7 @@ class Container(containers.DeclarativeContainer):
     jwt_service = providers.Factory(JWTService, jwt_security=jwt_security)
     auth_service = providers.Factory(
         AuthService,
-        uow=sqlalchemy_uow,
+        unit_of_work=unit_of_work,
         password_security=password_security,
         jwt_service=jwt_service,
     )
