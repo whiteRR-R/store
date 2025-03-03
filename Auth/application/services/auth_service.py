5e77c052437cb6d4147f376b1e07f5b869d1916a
whiteRR-R <whiterrs.zzz@gmail.com>
2025-03-03 10:03:15 +0500

    added method that updated user password

--- Auth/application/services/auth_service.py
+++ Auth/application/services/auth_service.py
@@ -1,12 +1,15 @@
+from domain.entities.user import User
 from domain.interface.uow.base_uow import BaseUnitOfWork
 from domain.interface.services.auth_service import AuthServiceInterface
 from domain.interface.services.jwt_service import JWTServiceInterface
 from domain.interface.repository.auth_repository import AuthRepositoryInterface
 from application.dtos.login_dto import UserLoginDTO
 from application.dtos.user_dto import UserDTO
+from application.dtos.reset_password_dto import ResetPasswordDTO
 from application.helpers.dtos import user_dto_to_user_entity
 from application.interface.security.password_security import PasswordSecurityInterface
 from application.exceptions import AlreadyExistsException, AuthException, UserNotFoundException
+from typing import Optional
 
 
 class AuthService(AuthServiceInterface):
@@ -37,7 +40,7 @@ class AuthService(AuthServiceInterface):
         await self.unit_of_work.register_new(new_user)
         await self.unit_of_work.commit()
 
-    async def verify_user_credentials(self, user_credentials: UserLoginDTO):
+    async def verify_user_credentials(self, user_credentials: UserLoginDTO) -> User:
         """Проверяет учетные данные пользователя."""
         existing_user = await self.auth_repository.find_by_username(user_credentials.username)
         password_bytes = user_credentials.password.encode()
@@ -55,16 +58,24 @@ class AuthService(AuthServiceInterface):
             raise AlreadyExistsException("Email already exception")
         return None
     
-    async def get_user_by_username(self, username: str):
+    async def get_user_by_username(self, username: str) -> Optional[User]:
         """Получает пользователя по username или вызывает исключение."""
         user = await self.auth_repository.find_by_username(username)
         if user is None:
             raise UserNotFoundException("User not found or invalid credentials")
         return user
      
-    async def get_user_by_email(self, email: str):
+    async def get_user_by_email(self, email: str) -> Optional[User]:
         """Получает пользователя по email или вызывает исключение."""
         user = await self.auth_repository.find_by_email(email)
         if user is None:
             raise UserNotFoundException("User not found or invalid credentials")
         return user
+
+    async def update_password(self, username: str, new_password: bytes):
+        """Обновляет пароль пользователя."""
+        user = await self.get_user_by_username(username)
+        hashed_password = self.password_security.get_hash_password(new_password)
+        user.update_password(hashed_password)
+        await self.unit_of_work.register_dirty(user)
+        await self.unit_of_work.commit()
