8229daa3827f9f5abdf95a1d350cef25e3cdf9f4
whiteRR-R <whiterrs.zzz@gmail.com>
2025-03-09 17:04:32 +0500

    refactor dependency injection in Auth container to improve session management and repository integration

--- Auth/container.py
+++ Auth/container.py
@@ -1,37 +1,41 @@
 from dependency_injector import containers, providers
-from config import config_manager
+from domain.entities.user import User
 from presentation.controllers.auth_controller import AuthController
 from application.usecase.auth_usecase import AuthUseCase
 from application.services.auth_service import AuthService
+from infrastructure.data_mapper.user_datamapper import UserDataMapper
+from infrastructure.repository.auth_repository import AuthRepository
 from infrastructure.persistence.database import Database
 from infrastructure.services.jwt_service import JWTService
 from infrastructure.uow.uow import UnitOfWork
 from infrastructure.security.jwt_security import JWTSecurity
 from infrastructure.security.password_security import PasswordSecurity
+from config import config_manager
 
 
 class Container(containers.DeclarativeContainer):
     database = providers.Singleton(Database, database_url=config_manager.database.DATABASE_URL)
+
+    session = providers.Resource(database.provided.session_factory)
     
-    # Асинхронный ресурс для работы сессии
-    session = providers.Resource(lambda database: database.get_session(), database=database)
-    print(session)
-    print(type(session))
-    unit_of_work = providers.Factory(
-        UnitOfWork,
-        session=session,
-    )
+    auth_repository = providers.Factory(AuthRepository, session=session)
+    user_datamapper = providers.Factory(UserDataMapper, session=session)
+    mappers = providers.Dict({User: user_datamapper})
     
+    unit_of_work = providers.Factory(UnitOfWork, session=session, mappers=mappers)
+    
+
     jwt_security = providers.Singleton(JWTSecurity)
     password_security = providers.Singleton(PasswordSecurity)
-    
+
     jwt_service = providers.Factory(JWTService, jwt_security=jwt_security)
     auth_service = providers.Factory(
         AuthService,
+        auth_repository=auth_repository,
         unit_of_work=unit_of_work,
         password_security=password_security,
         jwt_service=jwt_service,
     )
-    
+
     auth_usecase = providers.Factory(AuthUseCase, auth_service=auth_service, jwt_service=jwt_service)
     auth_controller = providers.Factory(AuthController, auth_usecase=auth_usecase)
