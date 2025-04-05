737eff597e6aed0d7a4361d553a450141e7e6ae3
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-05 18:45:35 +0500

    feat: implement SQLAlchemyDatabase class for async database interactions

---
+++ core/catalog/infrastructure/persistence/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+
+
+class SQLAlchemyDatabase:
+    def __init__(self, URL: str):
+        self._URL = URL
+        self._engine = create_async_engine(self._URL, echo=True)
+        self._session_maker = async_sessionmaker(self._engine, expire_on_commit=False)
+
+    async def get_session(self):
+        async with self._session_maker() as session:
+            yield session
+
+    async def close(self):
+        await self._engine.dispose()
+
+
