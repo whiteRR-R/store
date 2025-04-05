08e5c4cbba234e778c1e26b1019c13f2acd1e203
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-05 18:47:22 +0500

    feat: add CategoryDataMapper and CategoryRepository

---
+++ core/catalog/infrastructure/persistence/datamapper/category_mapper.py
@@ -0,0 +1,20 @@
+from domain.entities.category import Category
+from domain.value_objects.catalog_name import CategoryName
+from domain.value_objects.description import Description
+from infrastructure.persistence.models.category import CategoryModel
+
+
+class CategoryDataMapper:
+    def model_to_entity(self, category_model: CategoryModel):
+        return Category(
+            id=category_model.id,
+            name=CategoryName(category_model.name),
+            description=Description(category_model.description),
+        )
+    
+    def entity_to_model(self, category: Category):
+        return CategoryModel(
+            id=category.category_id,
+            name=category.name,
+            description=category.description,
+        )
---
+++ core/catalog/infrastructure/persistence/repository/category.py
@@ -0,0 +1,22 @@
+from domain.entities.category import Category
+from infrastructure.persistence.datamapper.category_mapper import CategoryDataMapper
+from infrastructure.persistence.models.category import CategoryModel
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import select
+
+class CategoryRepository:
+    def __init__(self, session: AsyncSession):
+        self._session = session
+        self._mapper  = CategoryDataMapper()
+
+    async def add(self, category: Category):
+        category_model = self._mapper.entity_to_model(category)
+        self._session.add(category_model)
+    
+    async def get_all(self) -> list[Category]:
+        categories = await self._session.execute(
+            select(CategoryModel)
+        )
+        return [self._mapper.model_to_entity(category) for category in categories.scalars().all()]
+
+
