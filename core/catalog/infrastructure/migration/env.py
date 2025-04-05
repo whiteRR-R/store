25736b9a1ed01a99e8e78f27488cdc1d701b0ebb
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-05 19:03:13 +0500

    feat: configure target metadata and database URL for migrations

--- core/catalog/infrastructure/migration/env.py
+++ core/catalog/infrastructure/migration/env.py
@@ -20,12 +20,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+from config import config_manager
+from infrastructure.persistence.database import Base
+
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", config_manager.database.DATABASE_URL)
 
 
 def run_migrations_offline() -> None:
