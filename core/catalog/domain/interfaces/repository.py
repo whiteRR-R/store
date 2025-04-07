7bc483442989c79ee69e29f15a8e97b55f830648
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-07 09:57:59 +0500

    feat: update CategoryRepositoryProtocol methods to be asynchronous

--- core/catalog/domain/interfaces/repository.py
+++ core/catalog/domain/interfaces/repository.py
@@ -3,10 +3,10 @@ from typing import Protocol
 
 
 class CategoryRepositoryProtocol(Protocol):
-    def add(self, category: Category):
+    async def add(self, category: Category):
         """Add a new category to the repository."""
         pass
     
-    def get_all(self) -> list[Category]:
+    async def get_all(self) -> list[Category]:
         """Retrieve all categories from the repository."""
         pass
