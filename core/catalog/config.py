55e9ee74582ac15bc213027a3877f93488b02765
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-07 09:00:37 +0500

    feat: update database configuration to use CATEGORY_DB_URL and enhance RabbitMQ settings

--- core/catalog/config.py
+++ core/catalog/config.py
@@ -6,14 +6,16 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent
-
+print(f"BASE_DIR: {BASE_DIR}")
 
 class DatabaseSettings(BaseModel):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("CATEGORY_DB_URL")
 
 
 class RabbitMQSettings(BaseModel):
-    ...
+    HOST: str = os.getenv("RABBITMQ_HOST")
+    EXCHANGE_NAME: str = os.getenv("RABBITMQ_EXCHANGE_NAME")
+    QUEUE_NAME: str = os.getenv("RABBITMQ_QUEUE_NAME")
 
 class ConfigManager(BaseSettings):
     database: DatabaseSettings = DatabaseSettings()
