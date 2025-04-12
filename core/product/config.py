9640a6a31b6605db890eab795526c8dfc31a7274
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-12 21:47:20 +0500

    feat: refactor configuration management to use Pydantic settings for database and RabbitMQ

--- core/product/config.py
+++ core/product/config.py
@@ -1,17 +1,41 @@
-import os
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import AmqpDsn, PostgresDsn
 
 
-class DatabaseConfig(BaseModel):
-    DATABASE_URL = os.getenv("DATABASE_URL", "")
+class DatabaseSettings(BaseSettings):
+    NAME: str
+    HOST: str
+    PORT: int
+    USER: str
+    PASS: str
+    URL: PostgresDsn
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="PRODUCT_POSTGRES_",
+        extra="ignore",
+    )
 
 
-class ConfigManager(BaseSettings):
-    database: DatabaseConfig = DatabaseConfig()
+class RabbitMQSettings(BaseSettings):
+    HOST: str
+    EXCHANGE_NAME: str
+    QUEUE_NAME: str
+    URL: AmqpDsn
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="RABBITMQ_",
+        extra="ignore",
+    )
+
+
+class ConfigManager:
+    def __init__(self):
+        self.database = DatabaseSettings()
+        self.rabbitmq = RabbitMQSettings()
 
 
 config_manager = ConfigManager()
