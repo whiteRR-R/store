a8ffe27dbade18a2904a5c313f27b659cfd6acbe
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-15 15:55:42 +0500

    fix: replace RabbitMQEventBus with PublisherEventBus in container setup

--- core/catalog/container.py
+++ core/catalog/container.py
@@ -4,7 +4,7 @@ from application.services.category_service import CategoryService
 from infrastructure.persistence.database import SQLAlchemyDatabase
 from infrastructure.persistence.repository.category import CategoryRepository
 from infrastructure.persistence.datamapper.category_mapper import CategoryDataMapper
-from infrastructure.event_bus.rabbitmq_event_bus import RabbitMQEventBus
+from infrastructure.event_bus.publisher import PublisherEventBus
 from config import config_manager
 
 
@@ -17,9 +17,9 @@ class Container(containers.Container):
         session=session,
     )
     
-    event_bus = providers.Singleton(
-        RabbitMQEventBus,
-        host=config_manager.rabbitmq.HOST,
+    pub_event_bus = providers.Singleton(
+        PublisherEventBus,
+        url=config_manager.rabbitmq.URL,
         exchange_name=config_manager.rabbitmq.EXCHANGE_NAME,
         queue_name=config_manager.rabbitmq.QUEUE_NAME,
         )
@@ -27,7 +27,7 @@ class Container(containers.Container):
     category_service = providers.Singleton(
         CategoryService,
         category_repository=category_repository,
-        event_bus=event_bus,
+        event_bus=pub_event_bus,
     )
     category_controller = providers.Factory(
         CategoryController,
