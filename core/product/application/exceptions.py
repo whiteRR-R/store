47707ab87a93af21bc922756ff1e11f105fa61f9
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:03:40 +0500

    feat: add custom exceptions for application error handling

---
+++ core/product/application/exceptions.py
@@ -0,0 +1,10 @@
+class ApplicationException(Exception):
+    """Base class for all application exceptions."""
+    pass
+
+class DataNotFoundException(ApplicationException):
+    """Exception raised when data is not found."""
+    pass
+
+
+
