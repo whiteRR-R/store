72a8cc13c125b0fb7696eadc1aa188dd65e4bcd2
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:11:19 +0500

    feat: add CategoryRepositoryProtocol for category management operations

---
+++ core/product/domain/interfaces/category_repository.py
@@ -0,0 +1,35 @@
+from domain.entities.category import Category
+from typing import Iterable, Protocol, List
+from uuid import UUID
+
+
+class CategoryRepositoryProtocol(Protocol):
+    async def create(self, category: Category) -> None:
+        """
+        Adds a new category to the repository.
+        """
+        ...
+    
+    async def delete(self, category: Category) -> None:
+        """
+        Delete category by id
+        """
+        ...
+    
+    async def get_all(self) -> List[Category]:
+        """
+        Retrieves all categories from the repository.
+        """
+        ...
+    
+    async def get_by_id(self, category_id: UUID) -> Category:
+        """
+        Gets a category by its ID.
+        """
+        ...
+    
+    async def get_by_ids(self, category_ids: Iterable[UUID]) -> List[Category]:
+        """
+        Gets categories by their IDs.
+        """
+        ...
