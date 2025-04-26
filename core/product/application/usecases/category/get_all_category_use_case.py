ad7c8d3e016f674406f79b84f5d31a3b9a80be0f
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:20:58 +0500

    feat: implement GetAllCategoryUseCase for retrieving all categories

---
+++ core/product/application/usecases/category/get_all_category_use_case.py
@@ -0,0 +1,12 @@
+from domain.entities.category import Category
+from domain.interfaces.category_repository import CategoryRepositoryProtocol
+from typing import List
+
+
+class GetAllCategoryUseCase:
+    def __init__(self, category_repository: CategoryRepositoryProtocol):
+        self.category_repository = category_repository
+    
+    async def execute(self) -> List[Category]:
+        categories = await self.category_repository.get_all()
+        return categories
