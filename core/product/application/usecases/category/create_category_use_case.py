c6af3e958e523d73efa5cc8698b8ba86c9956547
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:20:38 +0500

    feat: implement CreateCategoryUseCase for category creation logic

---
+++ core/product/application/usecases/category/create_category_use_case.py
@@ -0,0 +1,14 @@
+from domain.entities.category import Category
+from domain.interfaces.category_repository import CategoryRepositoryProtocol
+from application.dtos.category_dto import CategoryDTO
+from application.factories.category_factory import CategoryFactory
+
+
+class CreateCategoryUseCase:
+    def __init__(self, category_repository: CategoryRepositoryProtocol):
+        self.category_repository = category_repository
+    
+    async def execute(self, category_dto: CategoryDTO) -> None:
+        category = CategoryFactory.create_from_dto(category_dto)
+        await self.category_repository.create(category)
+
