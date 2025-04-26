e8c7a6daa66d308187c048bfd6a9d4bb005ed2f7
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 19:09:22 +0500

    refactor: reorganize ProductRepository methods and remove unused find methods

--- core/product/infrastructure/persistence/repository/product_repository.py
+++ core/product/infrastructure/persistence/repository/product_repository.py
@@ -1,31 +1,43 @@
-from sqlalchemy import select, update
+from domain.aggregates.product import ProductRoot
+from infrastructure.persistence.models.category_model import CategoryModel
+from infrastructure.persistence.models.brand_model import BrandModel
+from infrastructure.persistence.models.product_model import ProductModel
+from infrastructure.persistence.datamappers.product_mapper import ProductDataMapper
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import NoResultFound
-from domain.entities.product import Product
-from infrastructure.persistence.models.product import ProductModel
+from typing import List, Iterable, Optional
+from uuid import UUID
 
 
 class ProductRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
-
-    async def get_all(self):
+        self.mapper = ProductDataMapper()
+    
+    async def create(
+        self,
+        product: ProductRoot,
+        brand: BrandModel,
+        categories: Iterable[CategoryModel]
+    ) -> None:
+        
+        product_model = self.mapper.entity_to_model(product, brand, categories)
+        self.session.add(product_model)
+        await self.session.commit()
+    
+    async def delete(self, product: ProductModel) -> None:
+        await self.session.delete(product)
+        await self.session.commit()
+    
+    async def get_by_id(self, product_id: UUID) -> Optional[ProductRoot]:
+        stmt = await self.session.execute(
+            select(ProductModel).where(ProductModel.id == product_id)
+        )
+        product = stmt.scalars().one_or_none()
+        return self.mapper.model_to_entity(product) if product else None
+        
+    
+    async def get_all(self) -> List[ProductRoot]:
         stmt = await self.session.execute(select(ProductModel))
         products = stmt.scalars().all()
-        return products
-    
-    async def find_by_name(self, name: str):
-        stmt = await self.session.execute(
-            select(ProductModel)
-            .where(ProductModel.name == name)
-        )
-        product = stmt.scalar_one_or_none()
-        return product
-
-    async def find_by_category(self, category: str):
-        stmt = await self.session.execute(
-            select(ProductModel)
-            .where(ProductModel.category == category)
-        )
-        products = stmt.scalars().all()
-        return products
+        return [self.mapper.model_to_entity(product) for product in products]
