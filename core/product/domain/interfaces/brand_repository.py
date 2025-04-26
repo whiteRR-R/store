3f4422b6bfca76f690e1f449f4cfd129d6bf7a15
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:10:45 +0500

    feat: add BrandRepositoryProtocol for brand management operations

---
+++ core/product/domain/interfaces/brand_repository.py
@@ -0,0 +1,23 @@
+from domain.entities.brand import Brand
+from typing import Protocol
+from uuid import UUID
+
+
+class BrandRepositoryProtocol(Protocol):
+    async def add(self, brand: Brand) -> None:
+        """
+        Adds a new brand to the repository.
+        """
+        ...
+    
+    async def get_all(self) -> list[Brand]:
+        """
+        Retrieves all brands from the repository.
+        """
+        ...
+    
+    async def get_by_id(self, brand_id: UUID) -> Brand:
+        """
+        Gets a brand by its ID.
+        """
+        ...
