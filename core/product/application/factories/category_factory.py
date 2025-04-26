a1d88b877a3c3b274909414940826465c65a9b65
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 19:07:51 +0500

    feat: refactor CategoryFactory to include category_id in create method

--- core/product/application/factories/category_factory.py
+++ core/product/application/factories/category_factory.py
@@ -1,11 +1,21 @@
 from domain.entities.category import Category
 from domain.value_objects.category_name import CategoryName
 from application.dtos.category_dto import CategoryDTO
+from typing import Optional
+from uuid import UUID
 
 
 class CategoryFactory:
+    
     @staticmethod
-    def create_from_dto(category_dto: CategoryDTO):
+    def create(category_name: str, category_id: Optional[UUID] = None) -> Category:
+        return Category(
+            id=category_id,
+            category_name=CategoryName(category_name),
+        )
+    
+    @staticmethod
+    def create_from_dto(category_dto: CategoryDTO) -> Category:
         return Category(
             category_name=CategoryName(category_dto.name)
         )
