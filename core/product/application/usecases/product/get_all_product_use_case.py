42c61e33693d8b9aa95d39ec9e3a19f36154f054
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:21:36 +0500

    feat: implement GetAllProductUseCase for retrieving all products

---
+++ core/product/application/usecases/product/get_all_product_use_case.py
@@ -0,0 +1,12 @@
+from domain.interfaces.product_repository import ProductRepositoryProtocol
+from domain.aggregates.product import ProductRoot
+from typing import List
+
+
+class GetAllProductUseCase:
+    def __init__(self, product_repository: ProductRepositoryProtocol):
+        self.product_repository = product_repository
+        
+    async def execute(self) -> List[ProductRoot]:
+        products = await self.product_repository.get_all()
+        return products
