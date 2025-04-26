e7b370093b13666cf26697d69108c3847d67a24a
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 19:08:34 +0500

    feat: update ProductDataMapper to include necessary imports and enhance model_to_entity method

--- core/product/infrastructure/persistence/datamappers/product_mapper.py
+++ core/product/infrastructure/persistence/datamappers/product_mapper.py
@@ -1,4 +1,10 @@
 from domain.aggregates.product import ProductRoot
+from domain.value_objects.product_attribute import ProductAttribute
+from domain.value_objects.product_description import ProductDescription
+from domain.value_objects.product_name import ProductName
+from domain.value_objects.product_price import ProductPrice
+from application.factories.brand_factory import BrandFactory
+from application.factories.category_factory import CategoryFactory
 from infrastructure.persistence.models.product_model import ProductModel
 from infrastructure.persistence.models.brand_model import BrandModel
 from infrastructure.persistence.models.category_model import CategoryModel
@@ -14,7 +20,7 @@ class ProductDataMapper:
         
         ) -> ProductModel:
         """
-        Converts a model object to an entity object.
+        Converts a entity object to an model object.
         """
         return ProductModel(
             id=product.id,
@@ -26,7 +32,31 @@ class ProductDataMapper:
             categories=categories
         )
     
-    def model_to_entity(self):
-        ...
+    def model_to_entity(self, product_model: ProductModel) -> ProductRoot:
+        """
+        Converts a model object to an entity object.
+        """
+        attributes = [
+            ProductAttribute(key, value) for key, value in product_model.attributes.items()
+        ]
+        categories = [
+            CategoryFactory.create(category_id=category.id, category_name=category.name)
+            for category in product_model.categories
+        ]
+        brand = BrandFactory.create(
+            id=product_model.brand.id,
+            brand_name=product_model.brand.name
+        )
+        
+        return ProductRoot(
+            id=product_model.id,
+            name=ProductName(product_model.name),
+            brand=brand,
+            description=ProductDescription(product_model.description),
+            price=ProductPrice(product_model.price),
+            categories=categories,
+            attributes=attributes,
+        )
+        
     
     
