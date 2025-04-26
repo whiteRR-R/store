41a0f370ce47e4040a4e76e4a7cb512fce1be271
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:20:50 +0500

    feat: implement DeleteCategoryUseCase for category deletion logic

---
+++ core/product/application/usecases/category/delete_category_use_case.py
@@ -0,0 +1,14 @@
+from domain.interfaces.category_repository import CategoryRepositoryProtocol
+from application.exceptions import DataNotFoundException
+from uuid import UUID
+
+
+class DeleteCategoryUseCase:
+    def __init__(self, category_repository: CategoryRepositoryProtocol):
+        self.category_repository = category_repository
+    
+    async def execute(self, category_id: UUID) -> None:
+        category = await self.category_repository.get_by_id(category_id)
+        if not category:
+            raise DataNotFoundException(f"Category with {category_id} IDS not found")
+        await self.category_repository.delete(category)
