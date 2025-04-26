1a17a80fe245ad46a6a95102970da5adc6ac82f0
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-26 16:06:00 +0500

    feat: enhance ProductRoot class with exception handling for attributes and categories

--- core/product/domain/aggregates/product.py
+++ core/product/domain/aggregates/product.py
@@ -5,11 +5,12 @@ from domain.value_objects.product_image import ProductImage
 from domain.value_objects.product_attribute import ProductAttribute
 from domain.value_objects.product_description import ProductDescription
 from domain.value_objects.product_price import ProductPrice
-from domain.exceptions import AlreadyExistException
-from typing import Optional, List, MutableSequence
+from domain.exceptions import AlreadyExistException, NotFoundException
+from typing import List, Optional, Tuple, MutableSequence
+from uuid import uuid4, UUID
 
 
-class Product:
+class ProductRoot:
     def __init__(
         self,
         name: ProductName,
@@ -18,32 +19,50 @@ class Product:
         price: ProductPrice, 
         categories: MutableSequence[Category],
         attributes: MutableSequence[ProductAttribute],
-        images: MutableSequence[ProductImage],
+        id: Optional[UUID] = None,
+        # images: MutableSequence[ProductImage],
     ) -> None:
+        self._id = id or uuid4()
         self._name = name
         self._brand = brand
         self._description = description
         self._price = price
         self._categories = categories
         self._attributes = attributes
-        self._images = images  
+        # self._images = images  
         
-    def add_image(self, image: ProductImage) -> None:
-        self._images.append(image)
+    # def add_image(self, image: ProductImage) -> None:
+    #     self._images.append(image)
     
     def add_attribute(self, attribute: ProductAttribute) -> None:
+        if attribute in self._attributes:
+            raise AlreadyExistException("Attribute already exist")
         self._attributes.append(attribute)
+    
+    def remove_attribute(self, attribute: ProductAttribute) -> None:
+        if attribute not in self._attributes:
+            raise NotFoundException("Attribute does not exist")
+        self._attributes.remove(attribute)
     
     def add_category(self, category: Category) -> None:
         if category in self._categories:
             raise AlreadyExistException("Category already exist")
         self._categories.append(category)
     
+    def remove_category(self, category: Category) -> None:
+        if category not in self._categories:
+            raise NotFoundException("Category does not exist")
+        self._categories.remove(category)
+    
     def update_description(self, description: ProductDescription) -> None:
         self._description = description
     
     def update_price(self, price: ProductPrice):
         self._price = price
+    
+    @property
+    def id(self) -> UUID:
+        return self._id
     
     @property
     def name(self):
@@ -69,16 +88,16 @@ class Product:
     def attributes(self) -> List[ProductAttribute]:
         return [attribute for attribute in self._attributes]
     
-    @property
-    def images(self) -> List[ProductImage]:
-        return [image for image in self._images]
+    # @property
+    # def images(self) -> Tuple[ProductImage]:
+    #     return (image for image in self._images)
     
     def __repr__(self) -> str:
         return (
             f"<Product: {self.name}, "
             f"Brand: {self.brand}, "
             f"Price: {self.price}, "
-            f"Categories: {[category.name for category in self.categories]}>"
+            f"Categories: {(category.name for category in self.categories)}>"
         )
         
     
