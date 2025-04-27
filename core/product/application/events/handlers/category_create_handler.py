9417db04d3147673e61ec06b4b1472d949b51aa7
whiteRR-R <whiterrs.zzz@gmail.com>
2025-04-27 15:32:02 +0500

    feat: implement CategoryCreateHandler for handling category creation events

---
+++ core/product/application/events/handlers/category_create_handler.py
@@ -0,0 +1,16 @@
+from domain.interfaces.category_repository import CategoryRepositoryProtocol
+from application.events.integration.category_create_event import CategoryCreateEvent
+from application.factories.category_factory import CategoryFactory
+from uuid import UUID
+
+
+class CategoryCreateHandler:
+    def __init__(self, category_repository: CategoryRepositoryProtocol):
+        self.category_repository = category_repository
+
+    async def handle(self, event: CategoryCreateEvent):
+        category = CategoryFactory.create(
+            category_id=UUID(event.category_id),
+            category_name=event.category_name
+        )
+        await self.category_repository.create(category)
