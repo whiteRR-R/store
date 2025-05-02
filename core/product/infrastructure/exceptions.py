76a03e276ceffa72b1418d8c4c622f6843f1b0d6
whiteRR-R <whiterrs.zzz@gmail.com>
2025-05-02 18:27:36 +0500

    refactor: remove duplicate DataNotFoundException class definition in exceptions module

--- core/product/infrastructure/exceptions.py
+++ core/product/infrastructure/exceptions.py
@@ -7,7 +7,11 @@ class DatabaseConnectionError(InfrastructureException):
     """Exception raised when there is a database connection error."""
     pass
 
+class DataNotFoundException(InfrastructureException):
+    """Exception raised when data is not found in the database."""
+    pass
 
 class RollbackException(InfrastructureException):
     """Exception raised when a transaction rollback is required."""
     pass
+
