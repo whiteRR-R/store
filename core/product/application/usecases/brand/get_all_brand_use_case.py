f60880de0707bf1c34a34f0f68deedd9f89eb07f
whiteRR-R <whiterrs.zzz@gmail.com>
2025-05-23 21:25:10 +0500

    refactor: specify return type as List[BrandDTO] in execute method of GetAllBrandUseCase

--- core/product/application/usecases/brand/get_all_brand_use_case.py
+++ core/product/application/usecases/brand/get_all_brand_use_case.py
@@ -1,4 +1,4 @@
-from domain.entities.brand import Brand
+from application.dtos.brand_dto import BrandDTO
 from application.exceptions import DataNotFoundException
 from application.factories.brand_factory import BrandFactory
 from typing import List
@@ -8,7 +8,7 @@ class GetAllBrandUseCase:
     def __init__(self, brand_repository):
         self.brand_repository = brand_repository
 
-    async def execute(self):
+    async def execute(self) -> List[BrandDTO]:
         brands = await self.brand_repository.get_all()
         if not brands:
             raise DataNotFoundException("No brands found.")
