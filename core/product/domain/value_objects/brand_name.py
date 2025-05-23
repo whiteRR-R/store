d362fd6e9627b6c57da6229f24b7486512755432
whiteRR-R <whiterrs.zzz@gmail.com>
2025-05-23 20:14:32 +0500

    refactor: rename name attribute to value in BrandName and CategoryName classes for consistency

--- core/product/domain/value_objects/brand_name.py
+++ core/product/domain/value_objects/brand_name.py
@@ -11,16 +11,16 @@ class BrandName:
         name (str): The name of the brand.
     """
 
-    name: str
+    value: str
 
     def __post_init__(self):
-        if not self.name:
+        if not self.value:
             raise InvalidValueException("Brand name cannot be empty.")
-        if len(self.name) < 3:
+        if len(self.value) < 3:
             raise InvalidValueException("Brand name must be at least 3 characters long.")
-        if len(self.name) > 50:
+        if len(self.value) > 50:
             raise InvalidValueException("Brand name must be at most 50 characters long.")
-        if not self.name.isalnum():
+        if not self.value.isalnum():
             raise InvalidValueException("Brand name must be alphanumeric.")
-        if not self.name[0].isalpha():
+        if not self.value[0].isalpha():
             raise InvalidValueException("Brand name must start with a letter.")
