dad2bf9f334c66458cafd42dc9dd4a2dcd347c60
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-07 22:43:14 +0500

    feat: refactor CategoryRepository and ProductRepository to use session parameter in methods and apply transaction decorator

--- core/product/infrastructure/persistence/repository/category_repository.py
+++ core/product/infrastructure/persistence/repository/category_repository.py
@@ -1,6 +1,7 @@
 from domain.entities.category import Category
 from infrastructure.persistence.datamappers.category_mapper import CategoryDataMapper
 from infrastructure.persistence.models.category_model import CategoryModel
+from infrastructure.persistence.decorators import transaction
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Iterable, List, Optional, AsyncContextManager
 from sqlalchemy import select
@@ -12,28 +13,28 @@ class CategoryRepository:
         self.session_context_manager = session_context_manager
         self.mapper = CategoryDataMapper()
 
-    async def add(self, category: Category) -> None:
-        async with self.session_context_manager as session:
-            category_model = self.mapper.entity_to_model(category)
-            session.add(category_model)
-            await session.commit()
+    @transaction
+    async def add(self, session: AsyncSession, category: Category) -> None:
+        category_model = self.mapper.entity_to_model(category)
+        session.add(category_model)
+        await session.commit()
 
-    async def get_all(self) -> List[Category]:
-        async with self.session_context_manager as session:    
-            stmt = await session.execute(select(CategoryModel))
-            categories = stmt.scalars().all()
-            return [self.mapper.model_to_entity(category) for category in categories]
+    @transaction
+    async def get_all(self, session: AsyncSession) -> List[Category]:
+        stmt = await session.execute(select(CategoryModel))
+        categories = stmt.scalars().all()
+        return [self.mapper.model_to_entity(category) for category in categories]
 
-    async def get_by_id(self, category_id: UUID) -> Optional[Category]:
-        async with self.session_context_manager as session:
-            stmt = await session.execute(select(CategoryModel).where(CategoryModel.id == category_id))
-            category = stmt.scalars().first()
-            if category:
-                return self.mapper.model_to_entity(category)
-            return None
+    @transaction
+    async def get_by_id(self, session: AsyncSession, category_id: UUID) -> Optional[Category]:
+        stmt = await session.execute(select(CategoryModel).where(CategoryModel.id == category_id))
+        category = stmt.scalars().first()
+        if category:
+            return self.mapper.model_to_entity(category)
+        return None
 
-    async def get_by_ids(self, category_ids: Iterable[UUID]) -> List[Category]:
-        async with self.session_context_manager as session:
-            stmt = await session.execute(select(CategoryModel).where(CategoryModel.id.in_(category_ids)))
-            categories = stmt.scalars().all()
-            return [self.mapper.model_to_entity(category) for category in categories]
+    @transaction
+    async def get_by_ids(self, session: AsyncSession, category_ids: Iterable[UUID]) -> List[Category]:
+        stmt = await session.execute(select(CategoryModel).where(CategoryModel.id.in_(category_ids)))
+        categories = stmt.scalars().all()
+        return [self.mapper.model_to_entity(category) for category in categories]
--- core/product/infrastructure/persistence/repository/product_repository.py
+++ core/product/infrastructure/persistence/repository/product_repository.py
@@ -3,6 +3,7 @@ from infrastructure.persistence.models.category_model import CategoryModel
 from infrastructure.persistence.models.brand_model import BrandModel
 from infrastructure.persistence.models.product_model import ProductModel
 from infrastructure.persistence.datamappers.product_mapper import ProductDataMapper
+from infrastructure.persistence.decorators import transaction
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List, Iterable, Optional, AsyncContextManager
@@ -14,32 +15,33 @@ class ProductRepository:
         self.session_context_manager = session_context_manager
         self.mapper = ProductDataMapper()
     
+    @transaction
     async def add(
         self,
+        session: AsyncSession,
         product: ProductRoot,
         brand: BrandModel,
         categories: Iterable[CategoryModel]
     ) -> None:
-        async with self.session_context_manager as session:
-            product_model = self.mapper.entity_to_model(product, brand, categories)
-            session.add(product_model)
-            await session.commit()
+        product_model = self.mapper.entity_to_model(product, brand, categories)
+        session.add(product_model)
+        await session.commit()
     
-    async def delete(self, product: ProductModel) -> None:
-        async with self.session_context_manager as session:
-            await session.delete(product)
-            await session.commit()
+    @transaction
+    async def delete(self, session: AsyncSession, product: ProductModel) -> None:
+        await session.delete(product)
+        await session.commit()
     
-    async def get_by_id(self, product_id: UUID) -> Optional[ProductRoot]:
-        async with self.session_context_manager as session:
-            stmt = await session.execute(
-                select(ProductModel).where(ProductModel.id == product_id)
-            )
-            product = stmt.scalars().one_or_none()
-            return self.mapper.model_to_entity(product) if product else None
+    @transaction
+    async def get_by_id(self, session: AsyncSession, product_id: UUID) -> Optional[ProductRoot]:
+        stmt = await session.execute(
+            select(ProductModel).where(ProductModel.id == product_id)
+        )
+        product = stmt.scalars().one_or_none()
+        return self.mapper.model_to_entity(product) if product else None
     
-    async def get_all(self) -> List[ProductRoot]:
-        async with self.session_context_manager as session:
-            stmt = await session.execute(select(ProductModel))
-            products = stmt.scalars().all()
-            return [self.mapper.model_to_entity(product) for product in products]
+    @transaction
+    async def get_all(self, session: AsyncSession) -> List[ProductRoot]:
+        stmt = await session.execute(select(ProductModel))
+        products = stmt.scalars().all()
+        return [self.mapper.model_to_entity(product) for product in products]
