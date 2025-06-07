ee6c186b6fd84046135425a6386dcde4aebf3a86
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-07 21:05:41 +0500

    feat: change product_id type to UUID in GetByIdProductUseCase

--- core/product/application/usecases/product/get_by_id_product_use_case.py
+++ core/product/application/usecases/product/get_by_id_product_use_case.py
@@ -10,8 +10,8 @@ class GetByIdProductUseCase:
     def __init__(self, product_repository: ProductRepositoryProtocol):
         self.product_repository = product_repository
         
-    async def execute(self, product_id: str) -> ProductDTO:
-        product = await self.product_repository.get_by_id(UUID(product_id))
+    async def execute(self, product_id: UUID) -> ProductDTO:
+        product = await self.product_repository.get_by_id(product_id)
         if not product:
             raise DataNotFoundException(f"Product with {product_id} IDS not found")
         return ProductFactory.to_dto(product)
