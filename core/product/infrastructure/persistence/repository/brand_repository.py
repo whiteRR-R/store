d2c35c23de3f48824ad363bf98776368e95e5a79
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-07 21:25:17 +0500

    feat: refactor BrandRepository to use transaction decorator and improve session management

--- core/product/infrastructure/persistence/repository/brand_repository.py
+++ core/product/infrastructure/persistence/repository/brand_repository.py
@@ -1,34 +1,40 @@
 from domain.entities.brand import Brand
 from infrastructure.persistence.datamappers.brand_mapper import BrandDataMapper
 from infrastructure.persistence.models.brand_model import BrandModel
+from infrastructure.persistence.decorators import transaction
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from typing import List, Optional, AsyncContextManager
+from typing import List, Optional, Callable, AsyncContextManager
+from sqlalchemy import select, delete
 from uuid import UUID
 
 
 class BrandRepository:
-    def __init__(self, session_context_manager: AsyncContextManager[AsyncSession]) -> None:
-        self.session_context_manager = session_context_manager 
+    def __init__(self, session_context_manager: Callable[[], AsyncContextManager[AsyncSession]]) -> None:
+        self.session_context_manager = session_context_manager
         self.mapper = BrandDataMapper()
-        print(f"BrandRepository инициализирован с менеджером контекста сессии: {self.session_context_manager}")
 
-    async def add(self, brand: Brand) -> None:
-        async with self.session_context_manager as session: # Получаем актуальную сессию
-            brand_model = self.mapper.entity_to_model(brand)
-            session.add(brand_model)
-            await session.commit()
+    @transaction
+    async def add(self, session: AsyncSession, brand: Brand) -> None:
+        brand_model = self.mapper.entity_to_model(brand)
+        session.add(brand_model)
+        await session.commit()
 
-    async def get_all(self) -> List[Brand]:
-        async with self.session_context_manager as session:
-            stmt = await session.execute(select(BrandModel))
-            brands = stmt.scalars().all()
-            return [self.mapper.model_to_entity(brand) for brand in brands]
-        
-    async def get_by_id(self, brand_id: UUID) -> Optional[Brand]:
-        async with self.session_context_manager as session:
-            stmt = await session.execute(select(BrandModel).where(BrandModel.id == brand_id))
-            brand = stmt.scalars().first()
-            if brand:
-                return self.mapper.model_to_entity(brand)
-            return None
+    @transaction
+    async def get_all(self, session: AsyncSession) -> List[Brand]:        
+        stmt = await session.execute(select(BrandModel))
+        brands = stmt.scalars().all()
+        return [self.mapper.model_to_entity(brand) for brand in brands]
+    
+    @transaction
+    async def get_by_id(self, session: AsyncSession, brand_id: UUID) -> Optional[Brand]:
+        stmt = await session.execute(select(BrandModel).where(BrandModel.id == brand_id))
+        brand = stmt.scalars().first()
+        if brand:
+            return self.mapper.model_to_entity(brand)
+        return None
+
+    @transaction
+    async def delete(self, session: AsyncSession, brand: Brand) -> None:
+        brand_model = self.mapper.entity_to_model(brand)
+        await session.execute(delete(BrandModel).where(BrandModel.id == brand.id))
+        await session.commit()
