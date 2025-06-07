5fc66affa4c482b91ef6c2df302c0d517d0f4425
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-07 14:09:33 +0500

    refactor: clean up container.py by removing unnecessary whitespace and comments, and standardizing session context manager usage

--- core/product/container.py
+++ core/product/container.py
@@ -1,14 +1,9 @@
-# container.py
-
 from dependency_injector import containers, providers
-
-
 from config import config_manager
 from infrastructure.persistence.database import Database
 from infrastructure.persistence.repository.brand_repository import BrandRepository
 from infrastructure.persistence.repository.category_repository import CategoryRepository
 from infrastructure.persistence.repository.product_repository import ProductRepository
-
 from application.usecases.product.create_product_use_case import CreateProductUseCase
 from application.usecases.product.get_all_product_use_case import GetAllProductUseCase
 from application.usecases.product.get_by_id_product_use_case import GetByIdProductUseCase
@@ -19,7 +14,6 @@ from application.usecases.category.delete_category_use_case import DeleteCategor
 from application.usecases.brand.create_brand_use_case import CreateBrandUseCase
 from application.usecases.brand.get_all_brand_use_case import GetAllBrandUseCase
 from application.usecases.brand.delete_brand_use_case import DeleteBrandUseCase
-
 from infrastructure.event_bus.subscriber import EventBusSubscriber
 from application.events.handlers.category_create_handler import CategoryCreateHandler
 
@@ -36,9 +30,9 @@ class Container(containers.DeclarativeContainer):
     database = providers.Singleton(Database, database_url=config_manager.database.URL)
     session = providers.Factory(lambda database: database.get_session(), database) 
 
-    brand_repository    = providers.Factory(BrandRepository,    session_context_manager=session)
-    category_repository = providers.Factory(CategoryRepository, session=session)
-    product_repository  = providers.Factory(ProductRepository,  session=session)
+    brand_repository = providers.Factory(BrandRepository, session_context_manager=session)
+    category_repository = providers.Factory(CategoryRepository, session_context_manager=session)
+    product_repository = providers.Factory(ProductRepository, session_context_manager=session)
     
 
     event_bus_subscriber = providers.Singleton(
@@ -60,40 +54,47 @@ class Container(containers.DeclarativeContainer):
         category_repository=category_repository,
         brand_repository=brand_repository,
     )
+    
     get_all_product_use_case = providers.Factory(
         GetAllProductUseCase,
         product_repository=product_repository,
     )
+    
     get_by_id_product_use_case = providers.Factory(
         GetByIdProductUseCase,
         product_repository=product_repository,
     )
+    
     delete_product_use_case = providers.Factory(
         DeleteProductUseCase,
         product_repository=product_repository,
     )
-
+    
     create_category_use_case = providers.Factory(
         CreateCategoryUseCase,
         category_repository=category_repository,
     )
+    
     get_all_category_use_case = providers.Factory(
         GetAllCategoryUseCase,
         category_repository=category_repository,
     )
+    
     delete_category_use_case = providers.Factory(
         DeleteCategoryUseCase,
         category_repository=category_repository,
     )
-
+    
     create_brand_use_case = providers.Factory(
         CreateBrandUseCase,
         brand_repository=brand_repository,
     )
+    
     get_all_brand_use_case = providers.Factory(
         GetAllBrandUseCase,
         brand_repository=brand_repository,
     )
+    
     delete_brand_use_case = providers.Factory(
         DeleteBrandUseCase,
         brand_repository=brand_repository,
