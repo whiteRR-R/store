833bbbf4a09708bfd016ee88db6aa3d604722fe3
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-07 22:54:18 +0500

    refactor: remove unnecessary DataNotFoundException checks in GetAll use cases

--- core/product/application/usecases/brand/get_all_brand_use_case.py
+++ core/product/application/usecases/brand/get_all_brand_use_case.py
@@ -10,6 +10,4 @@ class GetAllBrandUseCase:
 
     async def execute(self) -> List[BrandDTO]:
         brands = await self.brand_repository.get_all()
-        if not brands:
-            raise DataNotFoundException("No brands found.")
         return [BrandFactory.to_dto(brand) for brand in brands]
