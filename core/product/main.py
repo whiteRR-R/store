08795dfa3dd9b29c7e81d42446854595bb23dc77
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-10 22:35:37 +0500

    feat: register exception handlers for DataNotFound, Rollback, InvalidValue, NotFound, and AlreadyExist exceptions

--- core/product/main.py
+++ core/product/main.py
@@ -1,8 +1,18 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from container import Container
+from domain.exceptions import InvalidValueException, NotFoundException, AlreadyExistException
+from application.exceptions import DataNotFoundException
+from infrastructure.exceptions import RollbackException
 from presentation.api.endpoints.product import router as product_router
 from presentation.api.endpoints.brand import router as brand_router
+from presentation.api.endpoints.category import router as category_router
+from presentation.api.exception_handlers import (
+    data_not_found_exception_handler,
+    rollback_exception_handler,
+    invalid_value_exception_handler,
+    not_found_exception_handler,
+    already_exist_exception_handler,
+)
 
 
 class Application:
@@ -14,8 +24,16 @@ class Application:
     def _include_routers(self):
         self.app.include_router(product_router, prefix="/api")
         self.app.include_router(brand_router, prefix="/api")
+        self.app.include_router(category_router, prefix="/api")
+    
+    def _registration_exception_handlers(self):
+        self.app.add_exception_handler(DataNotFoundException, data_not_found_exception_handler)
+        self.app.add_exception_handler(RollbackException, rollback_exception_handler)
+        self.app.add_exception_handler(InvalidValueException, invalid_value_exception_handler)
+        self.app.add_exception_handler(NotFoundException, not_found_exception_handler)
+        self.app.add_exception_handler(AlreadyExistException, already_exist_exception_handler)
 
-    async def initializate_handlers(self):
+    async def _initializate_handlers(self):
         event_bus_subscriber = self.container.event_bus_subscriber()
         category_create_handler = self.container.category_create_handler()
         event_bus_subscriber.register_handler(
@@ -24,7 +42,6 @@ class Application:
         )
 
     async def _lifespan(self, app: FastAPI):
-        self.container.init_resources()
         event_bus_subscriber = self.container.event_bus_subscriber()
         await event_bus_subscriber.connect()
         await self.initializate_handlers()
