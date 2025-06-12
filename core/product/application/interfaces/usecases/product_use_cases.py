338492e2b355a36f1533dd92757ebc680abde1f3
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-12 15:22:59 +0500

    fix: update CreateProductUseCaseProtocol to use CreateProductDTO and add attribute use case protocols

--- core/product/application/interfaces/usecases/product_use_cases.py
+++ core/product/application/interfaces/usecases/product_use_cases.py
@@ -1,10 +1,10 @@
 from typing import List, Protocol
-from application.dtos.product_dto import ProductDTO
+from application.dtos.product_dto import AttributeDTO, ProductDTO, CreateProductDTO
 from uuid import UUID
 
 
 class CreateProductUseCaseProtocol(Protocol):
-    async def execute(self, product_dto: ProductDTO) -> None:
+    async def execute(self, product_dto: CreateProductDTO) -> None:
         """Create a new product."""
         pass
     
@@ -21,4 +21,14 @@ class GetAllProductsUseCaseProtocol(Protocol):
 class GetProductByIdUseCaseProtocol(Protocol):
     async def execute(self, product_id: UUID) -> ProductDTO:
         """Retrieve a product by its ID."""
+        pass
+
+class AddProductAttributeUseCaseProtocol(Protocol):
+    async def execute(self, product_id: UUID, attribute_dto: AttributeDTO) -> None:
+        """Add an attribute to a product."""
+        pass
+
+class DeleteProductAttributeUseCaseProtocol(Protocol):
+    async def execute(self, product_id: UUID, attribute_dto: AttributeDTO) -> None:
+        """Delete an attribute from a product."""
         pass
