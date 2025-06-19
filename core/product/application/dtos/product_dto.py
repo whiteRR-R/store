e78566777a3817c3d7032db1d5878dbd7e87901c
whiteRR-R <whiterrs.zzz@gmail.com>
2025-06-19 21:12:50 +0500

    refactor: replace Pydantic models with dataclasses for product DTOs

--- core/product/application/dtos/product_dto.py
+++ core/product/application/dtos/product_dto.py
@@ -1,31 +1,38 @@
-from pydantic import BaseModel
+from typing import List, Optional, BinaryIO
 from uuid import UUID
-from typing import List, Optional
+from dataclasses import dataclass, field
 
 
-class AttributeDTO(BaseModel):
+@dataclass
+class AttributeDTO:
     key: str
     value: str
 
+@dataclass
+class Image:
+    file: BinaryIO
+    filename: str
 
-class CreateProductDTO(BaseModel):
+@dataclass
+class CreateProductDTO:
     name: str
     brand_id: UUID
     description: str
     price: int
     category_ids: List[UUID]
-    attributes: List[AttributeDTO]
+    attributes: List[AttributeDTO] = field(default_factory=list)
 
-
-class ProductDTO(CreateProductDTO):
+@dataclass
+class ProductDTO:
     id: UUID
     name: str
     brand_id: UUID
     description: str
     price: int
     category_ids: List[UUID]
-    attributes: List[AttributeDTO]
+    attributes: List[AttributeDTO] = field(default_factory=list)
+    images: List[str] = field(default_factory=list)
 
-
+@dataclass
 class UpdateProductDTO(ProductDTO):
-    ...
+    pass
