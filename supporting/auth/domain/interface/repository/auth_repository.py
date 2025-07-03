4626e55347e5f74106237981986eadc3f64fb236
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-03 14:26:54 +0500

    fix: rename methods in AuthRepositoryProtocol for consistency

--- supporting/auth/domain/interface/repository/auth_repository.py
+++ supporting/auth/domain/interface/repository/auth_repository.py
@@ -4,10 +4,10 @@ from domain.entities.user import User
 
 class AuthRepositoryProtocol(Protocol):
     """ Интерфейс репозитория для работы с пользователями. """
-    async def find_by_username(self, username: str):
-        """ Находит пользователя по его имени (username). """
+    async def get_by_username(self, username: str):
+        """ Отдает пользователя по его имени (username). """
         ...
 
-    async def find_by_email(self, email: str):
-        """ Находит пользователя по его почте (email). """
+    async def get_by_email(self, email: str):
+        """ Отдает пользователя по его почте (email). """
         ...
