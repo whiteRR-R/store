563c1710ba01f7c428aeabef63099f24bb9017e8
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-03 14:26:43 +0500

    feat: add UserFactory for user creation

---
+++ supporting/auth/domain/factories/user_factory.py
@@ -0,0 +1,18 @@
+from domain.entities.user import User
+from domain.valueobject.role import Role
+from domain.valueobject.email import Email
+from domain.valueobject.username import Username
+
+
+class UserFactory:
+    """ Фабрика для создания пользователей """
+
+    @classmethod
+    def create(self, username: str, role: str, email: str, hash_password: bytes) -> User:
+        """ Создает нового пользователя """
+        return User(
+            username=Username(username),
+            role=Role(role),
+            email=Email(email),
+            hash_password=hash_password
+        )
