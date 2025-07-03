e75d86e5aad36969f0577256677108456ae14695
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-03 14:55:18 +0500

    feat: implement context management methods in UnitOfWorkProtocol

--- supporting/auth/domain/interface/uow/base_uow.py
+++ supporting/auth/domain/interface/uow/base_uow.py
@@ -2,6 +2,18 @@ from typing import Protocol
 
 
 class UnitOfWorkProtocol(Protocol):  
+    async def __aenter__(self) -> 'UnitOfWorkProtocol':
+        """ Вход в контекст Unit of Work. """
+        ...
+    
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        """ Выход из контекста Unit of Work. """
+        if exc_type is None:
+            await self.commit()
+        else:
+            await self.rollback()
+        await self.clear()
+    
     async def register_new(self, obj) -> None:
         """ Регистрирует новый объект. """
         ...
