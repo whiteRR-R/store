e3845d4878545bab57d8c3ff0b7a36050e99e205
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-03 14:27:52 +0500

    refactor: update SQLAlchemyAuthRepository to use UserDataMapper and AsyncContextManager

--- supporting/auth/infrastructure/persistence/repository/auth_repository.py
+++ supporting/auth/infrastructure/persistence/repository/auth_repository.py
@@ -1,28 +1,35 @@
-from domain.entities.user import User
-from infrastructure.persistence.models.user_model import UserModel
+from typing import Optional, AsyncContextManager
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import NoResultFound
 from sqlalchemy import select, update
-from typing import Optional
+from domain.entities.user import User
+from infrastructure.persistence.data_mapper.user_datamapper import UserDataMapper
+from infrastructure.persistence.models.user_model import UserModel
 
 
-class AuthRepository:
+class SQLAlchemyAuthRepository:
     """ Инициализация Sqlalchemy Auth Repository репозитория. """
-    def __init__(self, session: AsyncSession):
-        self.session = session
-        
+    def __init__(
+        self,
+        user_data_mapper: UserDataMapper,
+        session_context_manager: AsyncContextManager[AsyncSession]
+    ):
+        self.user_data_mapper = user_data_mapper
+        self.session = session_context_manager
+    
     async def find_by_username(self, username: str) -> Optional[User]:
         """ Находит пользователя по его имени (username). """
-        stmt = await self.session.execute(
-            select(UserModel).where(UserModel.username == username)
-        )
-        user = stmt.scalar_one_or_none()
-        return user.to_entity() if user else None
-    
+        async with self.session as session:
+            stmt = await session.execute(
+                select(UserModel).where(UserModel.username == username)
+            )
+            user = stmt.scalar_one_or_none()
+            return self.user_data_mapper.to_entity(user) if user else None
+
     async def find_by_email(self, email: str) -> Optional[User]:
         """ Находит пользователя по его почте (email). """
-        stmt = await self.session.execute(
+        async with self.session as session:
+            stmt = await session.execute(
             select(UserModel).where(UserModel.email == email)
         )
         user = stmt.scalar_one_or_none()
-        return user.to_entity() if user else None
+        return self.user_data_mapper.to_entity(user) if user else None
