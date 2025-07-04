b01b5dcf00671773931204baeedc1f7f5e91a7ac
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-04 16:48:04 +0500

    refactor: consolidate exception classes into ValidationError for clarity

--- supporting/auth/domain/exceptions.py
+++ supporting/auth/domain/exceptions.py
@@ -2,17 +2,5 @@ class DomainException(Exception):
     pass
 
 
-class InvalidRoleException(DomainException):
-    pass
-
-
-class InvalidEmailException(DomainException):
-    pass
-
-
-class InvalidUsernameException(DomainException):
-    pass
-
-
-class InvalidPermissionException(DomainException):
+class ValidationError(DomainException):
     pass
