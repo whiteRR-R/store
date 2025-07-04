30775d672f3fc3c13fdd5b441af3c605d57d7c2b
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-04 20:36:55 +0500

    fix: add return type hints for get_by_username and get_by_email methods in AuthRepositoryProtocol

--- supporting/auth/domain/interface/repository/auth_repository.py
+++ supporting/auth/domain/interface/repository/auth_repository.py
@@ -17,10 +17,10 @@ class AuthRepositoryProtocol(Protocol):
         ...
     
     """ Интерфейс репозитория для работы с пользователями. """
-    async def get_by_username(self, username: str):
+    async def get_by_username(self, username: str) -> User:
         """ Отдает пользователя по его имени (username). """
         ...
 
-    async def get_by_email(self, email: str):
+    async def get_by_email(self, email: str) -> User:
         """ Отдает пользователя по его почте (email). """
         ...
