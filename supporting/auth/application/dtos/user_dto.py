8e69512f37a4d4b3e9883ef0521728a220f598e6
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-04 16:52:53 +0500

    fix: update UserDTO to use EmailStr for email validation and add password validation logic

--- supporting/auth/application/dtos/user_dto.py
+++ supporting/auth/application/dtos/user_dto.py
@@ -1,8 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr, field_validator
+from domain.exceptions import ValidationError
+import re
 
 
 class UserDTO(BaseModel):
     username: str
-    email: str
+    email: EmailStr
     password: bytes
     role: str = 'user'
+
+    @field_validator('password', mode='before')
+    @classmethod
+    def password_must_contain_letter_and_digit(cls, value: str) -> bytes:
+        if len(value) < 8:
+            raise ValidationError('Пароль должен быть не менее 8 символов')
+        if not re.search(r'\d', value):
+            raise ValidationError('Пароль должен содержать хотя бы одну цифру')
+        if not re.search(r'[A-Za-z]', value):
+            raise ValidationError('Пароль должен содержать хотя бы одну букву')
+        return value.encode('utf-8')
