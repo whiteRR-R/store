b0c859984b114c0049230b89529811db1d033521
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-04 16:53:05 +0500

    fix: change password type to str and update validation logic to raise ValueError

--- supporting/auth/application/dtos/user_dto.py
+++ supporting/auth/application/dtos/user_dto.py
@@ -1,21 +1,20 @@
-from pydantic import BaseModel, Field, EmailStr, field_validator
-from domain.exceptions import ValidationError
+from pydantic import BaseModel, EmailStr, field_validator
 import re
 
 
 class UserDTO(BaseModel):
     username: str
     email: EmailStr
-    password: bytes
+    password: str
     role: str = 'user'
 
     @field_validator('password', mode='before')
     @classmethod
-    def password_must_contain_letter_and_digit(cls, value: str) -> bytes:
+    def password_must_contain_letter_and_digit(cls, value: str) -> str:
         if len(value) < 8:
-            raise ValidationError('Пароль должен быть не менее 8 символов')
+            raise ValueError('Пароль должен быть не менее 8 символов')
         if not re.search(r'\d', value):
-            raise ValidationError('Пароль должен содержать хотя бы одну цифру')
+            raise ValueError('Пароль должен содержать хотя бы одну цифру')
         if not re.search(r'[A-Za-z]', value):
-            raise ValidationError('Пароль должен содержать хотя бы одну букву')
-        return value.encode('utf-8')
+            raise ValueError('Пароль должен содержать хотя бы одну букву')
+        return value
