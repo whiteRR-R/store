4d71f6d62e86f4d3c2e8dbb89043d01960dee944
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-13 12:38:50 +0500

    updated product_attribute value-object

--- core/product/domain/value_objects/product_attribute.py
+++ core/product/domain/value_objects/product_attribute.py
@@ -1,23 +1,26 @@
 from dataclasses import dataclass
+from uuid import UUID
 from domain.exceptions import InvalidValueException
 
 
 @dataclass(frozen=True)
 class ProductAttribute:
-    key: str
+    attribute_id: UUID
     value: str
 
     def __post_init__(self):
-        if not self.key:
-            raise InvalidValueException("Attribute name cannot be empty.")
-        if len(self.key) < 3:
-            raise InvalidValueException("Attribute name must be at least 3 characters long.")
-        if len(self.key) > 50:
-            raise InvalidValueException("Attribute name must be at most 50 characters long.")
-        if not self.key.isalnum():
-            raise InvalidValueException("Attribute name must be alphanumeric.")
-        if not self.key[0].isalpha():
-            raise InvalidValueException("Attribute name must start with a letter.")
+        if not isinstance(self.attribute_id, UUID):
+            raise InvalidValueException("Value is not UUID type")
+        if not self.value:
+            raise InvalidValueException("Attribute value cannot be empty.")
+        if len(self.value) < 3:
+            raise InvalidValueException("Attribute value must be at least 3 characters long.")
+        if len(self.value) > 50:
+            raise InvalidValueException("Attribute value must be at most 50 characters long.")
+        if not self.value.isalnum():
+            raise InvalidValueException("Attribute value must be alphanumeric.")
+        if not self.value[0].isalpha():
+            raise InvalidValueException("Attribute value must start with a letter.")
         if not self.value:
             raise InvalidValueException("Attribute value cannot be empty.")
 
