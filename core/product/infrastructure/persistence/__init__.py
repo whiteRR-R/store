670c0eb41d164718586835850eac82901fce60c1
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-13 12:45:04 +0500

    used alembic migrate

---
+++ core/product/infrastructure/persistence/__init__.py
@@ -0,0 +1,9 @@
+from infrastructure.persistence.models.product_model import ProductModel
+from infrastructure.persistence.models.category_model import CategoryModel
+from infrastructure.persistence.models.brand_model import BrandModel
+from infrastructure.persistence.models.image_model import ProductImageModel
+from infrastructure.persistence.models.attribute_model import AttributeModel
+from infrastructure.persistence.models.association_models import (
+    AssociationProductCategoryModel,
+    AssosiationProductAttributeModel
+)
