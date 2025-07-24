4a6ee1fffce8ae4711057d4f7b2fe2a7df916935
whiteRR-R <whiterrs.zzz@gmail.com>
2025-07-24 12:13:38 +0500

    deleted create_at sort field

--- core/product/application/dtos/filter_dto.py
+++ core/product/application/dtos/filter_dto.py
@@ -5,7 +5,6 @@ from enum import Enum
 class SortField(Enum):
     name = "name"
     price = "price"
-    create_at = "create_at"
 
 
 class SortOrder(Enum):
@@ -18,5 +17,5 @@ class ProductFilterDTO(BaseModel):
     max_price: int | None = Field(None, ge=0)
     limit: int = Field(20, ge=1, le=100)
     offset: int = Field(0, ge=0)
-    sort_by: SortField = Field(SortField.create_at)
+    sort_by: SortField = Field(SortField.price)
     order: SortOrder = Field(SortOrder.desc)
