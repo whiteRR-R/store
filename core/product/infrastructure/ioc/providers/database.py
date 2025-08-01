3c4d1f253f30606bf7a41447d41bebbe8d5c17e9
whiteRR-R <whiterrs.zzz@gmail.com>
2025-08-01 13:41:01 +0500

    added redis provider

--- core/product/infrastructure/ioc/providers/database.py
+++ core/product/infrastructure/ioc/providers/database.py
@@ -1,4 +1,5 @@
 from typing import AsyncIterable
+from redis.asyncio import Redis
 from dishka import Provider, Scope, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 from domain.interfaces.transaction_manager import TransactionManagerProcotol
@@ -7,13 +8,18 @@ from infrastructure.persistence.database.transaction_manager import SQLAlchemyTr
 from infrastructure.storage.s3_storage import S3ImageStorage
 
 class DatabaseProvider(Provider):
-    def __init__(self, database_url: str):
+    def __init__(self, database_url: str, redis_url: str):
         super().__init__()
         self._database = Database(database_url)
+        self._redis_url = redis_url
     
     @provide(scope=Scope.APP)
     def provide_database(self) -> Database:
         return self._database
+    
+    @provide(scope=Scope.APP, provides=Redis)
+    def provide_redis(self):
+        return Redis.from_url(url=self._redis_url)
         
     @provide(scope=Scope.REQUEST)
     async def provide_session(self, database: Database) -> AsyncIterable[AsyncSession]:
