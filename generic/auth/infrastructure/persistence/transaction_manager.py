bd08eb4beae42421f82564cfb435b93b49b96309
whiteRR-R <whiterrs.zzz@gmail.com>
2025-08-16 18:46:01 +0500

    feat: implement SQLAlchemyTransactionManager for async transaction handling

---
+++ generic/auth/infrastructure/persistence/transaction_manager.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class SQLAlchemyTransactionManager:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
